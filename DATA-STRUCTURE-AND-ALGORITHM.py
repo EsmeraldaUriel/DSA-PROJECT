8644b31f191f4c44cefcb9b783ccc47960d26a0b
EsmeraldaUriel <116430431+EsmeraldaUriel@users.noreply.github.com>
2023-02-10 19:03:27 +0800

    "Feature of the display to continue while using it"

--- DATA-STRUCTURE-AND-ALGORITHM.py
+++ DATA-STRUCTURE-AND-ALGORITHM.py
@@ -218,6 +218,34 @@ def findEnd(maze, moves):
         return True
     return False
 
+
+def solve_maze(maze, start, end):
+    row, col = len(maze), len(maze[0])
+    visited = [[False for j in range(col)] for i in range(row)]
+    path = []
+    directions = ["East", "North", "West", "South"]
+
+    def dfs(row, col, direction):
+        if row == end[0] and col == end[1]:
+            path.append(direction)
+            return True
+        if row < 0 or row >= len(maze) or col < 0 or col >= len(maze[0]):
+            return False
+        if visited[row][col]:
+            return False
+        if maze[row][col] == "#":
+            return False
+        visited[row][col] = True
+        path.append(direction)
+        if dfs(row + 1, col, "South") or dfs(row, col - 1, "West") or dfs(row - 1, col, "North") or dfs(row, col + 1, "East"):
+            return True
+        path.pop()
+        return False
+
+    if dfs(start[0], start[1], ""):
+        return path
+    return None
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -263,7 +291,11 @@ while running:
 
     # Start the main game loop
     main_loop(screen, pygame_maze)
-    pygame.display.update()
 
+    # Clear the screen
+    screen.fill(white)
+
+    # Update the screen
+    pygame.display.update()
 # Quit Pygame
 pygame.quit()
